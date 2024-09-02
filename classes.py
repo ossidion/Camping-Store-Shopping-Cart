467a1297bf6f0e6753df6e2f45c19e3b76203780
Alexander Graham <alexinbristol@outlook.com>
2024-09-02 13:46:23 +0100

    Updated commentary inline with PEP 8 and PEP 257.

--- classes.py
+++ classes.py
@@ -2,38 +2,46 @@
 """
 from tabulate import tabulate
 
-
-
 class Item:
 
     """The Item class accounts for each item that the customer
     chooses to put into their shopping cart. Each instance
-    is an item which can be added to the shopping cart. This
-    item clsss includes the following methods:
+    is an item which can be added to the shopping cart. The class is
+    initialized with the following instance variables:
+        - name of item
+        - price of item
+        - stock count of item
+        - quantity of item, which can be used as a counter for the number
+          of this item type the user has within their shopping cart.)
+
+    This item clsss includes the following methods:
+        - def __str__(self):
+        - def __repr__(self):
+        - def add_to_cart(self):
+        - def remove_from_cart(self):
+
     """
 
-    # __init__(): which initialises the instance item with a name,
-    # price, stock count and quantity, which can be used as a
-    # counter for the number of this item type the user has within
-    # their shopping cart.
     def __init__(self, name, price, stock, quantity=0):
         self.name = name
         self.price = price
         self.stock = stock
         self.quantity = quantity
-        # self.available_products = []
 
-    # __str__(): returns a string representation of an Item object.
     def __str__(self):
+        """Returns a string representation of an Item object and its
+        attributes.
+        """
         return (f"Item: {self.name}\n"
                 +f"Price: {self.price}\n"
                 +f"Stock Count: {self.stock}\n"
                 +f"Quantity in Shopping Carts: {self.quantity}")
     
-    def __repr__(self):
-        return print(f"Item: {self.name}\t"
-              +f"${self.price}\t"
-              +f"Stock: {self.stock}\n")
+    # def __repr__(self):
+    #     return print(f"Item: {self.name}, "
+    #           +f"Price: ${self.price}, "
+    #           +f"Stock: {self.stock}, "
+    #           +f"Quantity: {self.quantity}, ")
 
     def add_to_cart(self):
         if self.stock > 0:
